4c8b160ce250764ed6dd2ff41dc8dbc1d82d818f
jeferson <jefersoun.silva@gmail.com>
2024-03-29 20:32:03 -0300

    fix: update port

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ EOF
 }
 
 # Defina um serviço ECS
-resource "aws_ecs_service" "my_service" {
+resource "aws_ecs_service" "ecs_service" {
   name            = "ecs-service"
   cluster         = aws_ecs_cluster.ecs_cluster.arn
   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
@@ -76,7 +76,7 @@ resource "aws_ecs_service" "my_service" {
   load_balancer {
     target_group_arn = aws_lb_target_group.ecs_target_group.arn
     container_name   = "ecs-container"
-    container_port   = 80
+    container_port   = 8080
   }
 }
 
