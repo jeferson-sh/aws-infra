5b9f4cb57d09c5db0c1b59d6b229f624c11a53be
jeferson <jefersoun.silva@gmail.com>
2024-03-29 20:56:23 -0300

    fix: include index

--- main.tf
+++ main.tf
@@ -69,8 +69,8 @@ EOF
 resource "aws_ecs_service" "ecs_service" {
   count = length(aws_ecs_service.ecs_service) == 0 ? 1 : 0
   name            = "ecs-service"
-  cluster         = aws_ecs_cluster.ecs_cluster.arn
-  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+  cluster         = aws_ecs_cluster.ecs_cluster[0].arn
+  task_definition = aws_ecs_task_definition.ecs_task_definition[0].arn
   desired_count   = 1
 
   # Configurações para Load Balancer
@@ -80,7 +80,7 @@ resource "aws_ecs_service" "ecs_service" {
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.ecs_target_group.arn
+    target_group_arn = aws_lb_target_group.ecs_target_group[0].arn
     container_name   = "ecs-container"
     container_port   = 8080
   }
