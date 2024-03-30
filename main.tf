ff386ed61dae869eebc687a3a2cacfb575066e4a
jeferson <jefersoun.silva@gmail.com>
2024-03-29 23:13:43 -0300

    TEst deploy

--- main.tf
+++ main.tf
@@ -14,51 +14,275 @@ terraform {
 
 # Crie um repositório ECR
 resource "aws_ecr_repository" "ecr_repository" {
-  #count = length(aws_ecr_repository.ecr_repository) == 0 ? 1 : 0
   name = "ecr-repository"  # Nome do repositório ECR
 }
 
 # Crie um cluster ECS
 resource "aws_ecs_cluster" "ecs_cluster" {
-  #count = length(aws_ecs_cluster.ecs_cluster) == 0 ? 1 : 0
   name = "ecs-cluster"  # Nome do cluster
 }
 
 # Defina a IAM Role para as tasks ECS
 resource "aws_iam_role" "ecs_task_execution_role" {
-  #count = length(aws_iam_role.ecs_task_execution_role) == 0 ? 1 : 0
   name               = "ecs-task-execution-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+  assume_role_policy = jsonencode(
     {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ecs-tasks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": {
+            "Service": "ecs-tasks.amazonaws.com"
+          },
+          "Action": "sts:AssumeRole"
+        }
+      ]
     }
-  ]
-}
-EOF
+  )
+  managed_policy_arns = [ "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy" ]
 }
 
-# Anexe a política ECS à IAM Role
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
-  #count = length(aws_iam_role_policy_attachment.ecs_task_execution_role_policy_attachment) == 0 ? 1 : 0
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  role       = aws_iam_role.ecs_task_execution_role.name
+
+resource "aws_iam_role" "task_role" {
+  name               = "task-role"
+  assume_role_policy = jsonencode(
+    {
+      Version   = "2012-10-17",
+      Statement = [
+        {
+          Effect    = "Allow",
+          Principal = {
+            Service = "ecs-tasks.amazonaws.com"
+          },
+          Action    = "sts:AssumeRole"
+        }
+      ]
+    }
+  )
 }
+
+resource "aws_iam_policy" "ecs_policy" {
+  name        = "ecs_policy"
+  description = "Policy for reading from DynamoDB table"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "ECSTaskManagement",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:AttachNetworkInterface",
+                "ec2:CreateNetworkInterface",
+                "ec2:CreateNetworkInterfacePermission",
+                "ec2:DeleteNetworkInterface",
+                "ec2:DeleteNetworkInterfacePermission",
+                "ec2:Describe*",
+                "ec2:DetachNetworkInterface",
+                "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+                "elasticloadbalancing:DeregisterTargets",
+                "elasticloadbalancing:Describe*",
+                "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+                "elasticloadbalancing:RegisterTargets",
+                "route53:ChangeResourceRecordSets",
+                "route53:CreateHealthCheck",
+                "route53:DeleteHealthCheck",
+                "route53:Get*",
+                "route53:List*",
+                "route53:UpdateHealthCheck",
+                "servicediscovery:DeregisterInstance",
+                "servicediscovery:Get*",
+                "servicediscovery:List*",
+                "servicediscovery:RegisterInstance",
+                "servicediscovery:UpdateInstanceCustomHealthStatus"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "AutoScaling",
+            "Effect": "Allow",
+            "Action": [
+                "autoscaling:Describe*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "AutoScalingManagement",
+            "Effect": "Allow",
+            "Action": [
+                "autoscaling:DeletePolicy",
+                "autoscaling:PutScalingPolicy",
+                "autoscaling:SetInstanceProtection",
+                "autoscaling:UpdateAutoScalingGroup",
+                "autoscaling:PutLifecycleHook",
+                "autoscaling:DeleteLifecycleHook",
+                "autoscaling:CompleteLifecycleAction",
+                "autoscaling:RecordLifecycleActionHeartbeat"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "Null": {
+                    "autoscaling:ResourceTag/AmazonECSManaged": "false"
+                }
+            }
+        },
+        {
+            "Sid": "AutoScalingPlanManagement",
+            "Effect": "Allow",
+            "Action": [
+                "autoscaling-plans:CreateScalingPlan",
+                "autoscaling-plans:DeleteScalingPlan",
+                "autoscaling-plans:DescribeScalingPlans",
+                "autoscaling-plans:DescribeScalingPlanResources"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "EventBridge",
+            "Effect": "Allow",
+            "Action": [
+                "events:DescribeRule",
+                "events:ListTargetsByRule"
+            ],
+            "Resource": "arn:aws:events:*:*:rule/ecs-managed-*"
+        },
+        {
+            "Sid": "EventBridgeRuleManagement",
+            "Effect": "Allow",
+            "Action": [
+                "events:PutRule",
+                "events:PutTargets"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "events:ManagedBy": "ecs.amazonaws.com"
+                }
+            }
+        },
+        {
+            "Sid": "CWAlarmManagement",
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:DeleteAlarms",
+                "cloudwatch:DescribeAlarms",
+                "cloudwatch:PutMetricAlarm"
+            ],
+            "Resource": "arn:aws:cloudwatch:*:*:alarm:*"
+        },
+        {
+            "Sid": "ECSTagging",
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateTags"
+            ],
+            "Resource": "arn:aws:ec2:*:*:network-interface/*"
+        },
+        {
+            "Sid": "CWLogGroupManagement",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:DescribeLogGroups",
+                "logs:PutRetentionPolicy"
+            ],
+            "Resource": "arn:aws:logs:*:*:log-group:/aws/ecs/*"
+        },
+        {
+            "Sid": "CWLogStreamManagement",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogStream",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "arn:aws:logs:*:*:log-group:/aws/ecs/*:log-stream:*"
+        },
+        {
+            "Sid": "ExecuteCommandSessionManagement",
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeSessions"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "ExecuteCommand",
+            "Effect": "Allow",
+            "Action": [
+                "ssm:StartSession"
+            ],
+            "Resource": [
+                "arn:aws:ecs:*:*:task/*",
+                "arn:aws:ssm:*:*:document/AmazonECS-ExecuteInteractiveCommand"
+            ]
+        },
+        {
+            "Sid": "CloudMapResourceCreation",
+            "Effect": "Allow",
+            "Action": [
+                "servicediscovery:CreateHttpNamespace",
+                "servicediscovery:CreateService"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "ForAllValues:StringEquals": {
+                    "aws:TagKeys": [
+                        "AmazonECSManaged"
+                    ]
+                }
+            }
+        },
+        {
+            "Sid": "CloudMapResourceTagging",
+            "Effect": "Allow",
+            "Action": "servicediscovery:TagResource",
+            "Resource": "*",
+            "Condition": {
+                "StringLike": {
+                    "aws:RequestTag/AmazonECSManaged": "*"
+                }
+            }
+        },
+        {
+            "Sid": "CloudMapResourceDeletion",
+            "Effect": "Allow",
+            "Action": [
+                "servicediscovery:DeleteService"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "Null": {
+                    "aws:ResourceTag/AmazonECSManaged": "false"
+                }
+            }
+        },
+        {
+            "Sid": "CloudMapResourceDiscovery",
+            "Effect": "Allow",
+            "Action": [
+                "servicediscovery:DiscoverInstances",
+                "servicediscovery:DiscoverInstancesRevision"
+            ],
+            "Resource": "*"
+        }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_dynamodb_policy" {
+  role       = aws_iam_role.task_role.name
+  policy_arn = aws_iam_policy.ecs_policy.arn
+}
+
 
 # Defina uma definição de task ECS
 resource "aws_ecs_task_definition" "ecs_task_definition" {
-  #count = length(aws_ecs_task_definition.ecs_task_definition) == 0 ? 1 : 0
   family                   = "ecs-task-definition"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 256
-  memory                   = 512
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn = aws_iam_role.task_role.arn
   container_definitions    = <<EOF
 [
   {
