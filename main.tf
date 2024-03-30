374ee5cd1076c3bf1eefce6e5966139b4e459e4b
jeferson <jefersoun.silva@gmail.com>
2024-03-30 12:54:52 -0300

    update desired tasks

--- main.tf
+++ main.tf
@@ -307,7 +307,7 @@ resource "aws_ecs_service" "ecs_service" {
   name            = "ecs-service"
   cluster         = aws_ecs_cluster.ecs_cluster.arn
   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
-  desired_count   = 0
+  desired_count   = 2
   launch_type     = "FARGATE"  # Indica que o serviço será executado no Fargate
 
   # Configurações para Load Balancer
