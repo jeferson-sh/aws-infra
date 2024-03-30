b00055cbfe6bc99112b5f3f04b10feb0e2d2c442
jeferson <jefersoun.silva@gmail.com>
2024-03-29 22:39:39 -0300

    include memory and cpu

--- main.tf
+++ main.tf
@@ -57,6 +57,8 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   family                   = "ecs-task-definition"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
   container_definitions    = <<EOF
 [
   {
